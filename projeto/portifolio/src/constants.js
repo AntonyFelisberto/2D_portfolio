5e8cb3789526df9f026b4616c77cc81523ff99ec
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2024-04-12 18:12:46 -0300

    ajustes projeto

--- projeto/portifolio/src/constants.js
+++ projeto/portifolio/src/constants.js
@@ -1,11 +1,13 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc:"",
-    tv:"",
-    bed:"",
-    resume: "",
-    projects: "",
-    library: "",
-    exit: "",
+    pc:`Meu pc, lugar onde costumo ler os meus mangas e quadrinhos, alem de realizar meus projetos e estudos.`,
+    tv:`A minha TV, lugar onde assisto series, filmes, animes e videos como tutoriais do youtube.`,
+    bed:`Minha cama, lugar onde deito para descansar e dormir.`,
+    "cs-degree": `Meu certificado, Me graduei em Analise e desenvolvimento de sistemas em 2022, agora busco estudar ingles para morar em outros paises.`,
+    "sofa-table": "Meu sofa, lugar onde me sento para assistir TV e relaxar.",
+    resume: `Meu curriculo, criei um site para exibir meus certificados e mais sobre min, se quiser ver mais <a href="https://portifolio-sfn5.vercel.app/" target="_blank">Clique Aqui.</a>`,
+    projects: `Meus projetos, frutos dos meus estudo, coloco todos eles no meu GitHub e quando consigo realizo o deploy para disponibilizar eles online.<a href="https://github.com/AntonyFelisberto?tab=repositories" target="_blank">Clique Aqui para ver meu GitHub.</a>.`,
+    library: `A biblioteca, o lugar onde eu guardo alguns dos meus livros, mangas e quadrinhos.`,
+    exit: `A saida, se quiser deixar o projeto, apenas clique para fechar a aba.`,
 }
